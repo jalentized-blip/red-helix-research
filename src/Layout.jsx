331acdfc7ea784a1f531b7086ce9e8e8648f36db
jake boen <jalentized@gmail.com>
2026-01-23 09:00:21 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -51,7 +51,7 @@ export default function Layout({ children }) {
             <img 
               src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6972f2b59e2787f045b7ae0d/e486eaa24_thisisitbuddy.png" 
               alt="Red Dirt Research" 
-              className="h-16 w-auto object-contain"
+              className="h-20 w-auto object-contain"
             />
           </div>
 
